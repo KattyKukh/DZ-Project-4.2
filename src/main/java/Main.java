c9302c40a5041545b2217f7a1758034a50236e7b
Кухоцкая Екатерина <ekukhotskaya@mail.ru>
2022-05-31 14:19:11 +0300

    fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,14 @@
 public class Main {
-  public static void main(String[] args) {
-    BonusService service = new BonusService();
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
 
-    long amount = 1000_60;
-    boolean registered = true;
+        long amount = 1000_60;
+        boolean registered = true;
 
-    service.calculate(amount, registered);
-  }
+        long expected = 30;
+        long actual = service.calculate(amount, registered);
+        boolean check = expected == actual;
+        System.out.println(check);
+
+    }
 }
-
